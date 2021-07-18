b195f6bb1d587ed11289afb669bf23d6f6b58720
Antipa00 <87610746+Antipa00@users.noreply.github.com>
2021-07-18 20:26:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream> //вводим необходимые библиотеки
+#include <iostream> //Вводим нужные библиотеки
 #include <string>
 #include <map>
 #include <fstream>
@@ -13,15 +13,18 @@ int main (int argc, char *argv[])
   wordmap words;
   string curstring;
   bool label = false;
-  ifstream infile ("D:/Qt/Praktika_1_Antipin/Test1.txt"); //Открываем исходный файл, в котором нужно найти и удалить совпадения
+  ifstream infile ("D:/Qt/Praktika_1_Antipin/Test1.txt"); //указываем файл, из которого нужно удалить совпадения
+  fstream Output;
+  string Outfile = "D:/Qt/Praktika_1_Antipin/Test2.txt"; //указываем файл, куда нужно сохранить результат
+  Output.open(Outfile);
   if( ! infile )
      {
-         cerr << "Error in opening file."; //В случае ошибки выводим это для пользовал=теля
+         cerr << "Error in opening file."; //в случае ошибки, сообщаем об этом пользователю
          return -1;
      }
   while ( infile >> curstring )
   {
-      words[curstring]++;
+      words[curstring]++; //С помощью цикла подсчитываем количество слов в файле
   }
 
 
@@ -29,21 +32,24 @@ int main (int argc, char *argv[])
 
          for ( const_mapiter it = words.begin() ; it != words.end(); it++ )
           {
-              if ( (*it).second > 1 )  //проверяем наличие совпадений в файле
+              if ( (*it).second > 1 ) //Ищем совпадения в файле
                     {
                         label = true;
-                        cout //в случаи нахождения совпадений выводим их пользователю
+                        cout
                             <<         " Occurs "
                             <<         (*it).second
                             <<         " times "
                             <<         " word  :"
                             <<         (*it).first
                             <<         std::endl;
+                        Output << (*it).first<<endl; //Записываем совпадения в файл, только уже в единичном варианте
+
                     }
           }
 
-         if( ! label ) // в случае, если совпадений нет, выводим это пользователю
-            cout << " do not repeat.\n";
-
+         if( ! label ){
+            cout << " do not repeat.\n"; //в случае отсутствия совпадений выводим данное сообщение
+         }
+      system("pause");
       return 0;
 }
